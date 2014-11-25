85d9cfb2d9fbedc86b333ff82d2a75453c4445ef
Шум Константин <k.shym@ya.ru>
2014-11-25 12:45:03 +0300

    ДЗ 7: каркас проекта

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -8,5 +8,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^list/$', TemplateView.as_view(template_name='list.html'), name='list'),
+    url(r'^list/item/$', TemplateView.as_view(template_name='item.html'), name='item'),
     url(r'^admin/', include(admin.site.urls)),
 )
