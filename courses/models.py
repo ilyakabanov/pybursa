8c2689b2a5be19a1236c503e16e0ceadb83a294f
Oleksandr Berezovskyi <berezovskyi.oleksandr@gmail.com>
2014-12-09 15:36:44 +0200

    HW#11 completed

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from coaches.models import Coach
+from students.models import Address
 
 
 class Course(models.Model):
@@ -10,12 +11,14 @@ class Course(models.Model):
                          ('j', "JavaScript"))
     name = models.CharField(max_length=255)
     description = models.TextField()
-    coach = models.ForeignKey(Coach)
-    assistant = models.ForeignKey(Coach, blank=True,
-                                  related_name="course_assistant")
+    coach = models.ForeignKey('coaches.Coach')
+    assistant = models.ForeignKey('coaches.Coach', blank=True,
+                                  related_name="assistant", null=True)
     start_date = models.DateField()
     end_date = models.DateField()
     technology = models.CharField(max_length=255, choices=TECHNOLOGY_CHOICE)
+    venue = models.ForeignKey('students.Address', null=True, blank=True)
 
-    def __str__(self):
-        return "%s (%s) - %s" % (self.name, self.coach, self.technology)
+    def __unicode__(self):
+        return "%s (%s) - %s" % (self.name, self.coach,
+                                 self.get_technology_display())
